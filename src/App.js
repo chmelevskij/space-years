237f7f809301e29019b1563919982124fcfbe8ea
Tomas Chmelevskij <T.Chmelevskij@gmail.com>
2017-09-29 11:34:00 +0100

    Format

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import YearTable from './YearTable.js'
 import './App.css';
 
 const ORBITS = {
-      EARTH: 365.25,
-      MERCURY: 0.2408467,
-      VENUS: 0.61519726,
-      MARS: 1.8808158,
-      JUPITER: 11.862615,
-      SATURN: 29.447498,
-      URANUS: 84.016846,
-      NEPTUNE: 164.79132,
-      PLUTO: 248.00
-    }
+  EARTH: 365.25,
+  MERCURY: 0.2408467,
+  VENUS: 0.61519726,
+  MARS: 1.8808158,
+  JUPITER: 11.862615,
+  SATURN: 29.447498,
+  URANUS: 84.016846,
+  NEPTUNE: 164.79132,
+  PLUTO: 248.00
+}
 
 class App extends Component {
 
@@ -53,9 +53,9 @@ class App extends Component {
           <h1 className="App-title">Get Your Age!</h1>
           <EarthInput calculateYears={this.calculateYears} />
         </header>
-          <main>
+        <main>
           <YearTable planets={planets} />
-          </main>
+        </main>
       </div>
     );
   }
